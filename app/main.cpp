bdd78b05365e1894621b1559e8b7fc3327bc945b
mllimo <ar.guijarro.contreras@gmail.com>
2023-07-20 01:45:10 +0100

    Add a crazy triangle rotating

--- app/main.cpp
+++ app/main.cpp
@@ -1,5 +1,6 @@
 ﻿#include <iostream>
 #include <memory>
+#include <cmath>
 
 #include <Core/h/Vertex.h>
 #include <Core/h/VertexVector.h>
@@ -20,6 +21,11 @@ float Normalice(float max, float current)
 {
 	float relative = (2 * current) / max;
 	return -1.f + relative;
+}
+
+float Degrees2Rads(float degrees)
+{
+	return degrees * (3.14f / 180.f);
 }
 
 int main()
@@ -54,26 +60,46 @@ int main()
 
 	glViewport(0, 0, 800, 800);
 
-	easy::graphics::shapes::Triangle t1({ -1, -1, 0}, { 1, -1, 0}, { 0, 1, 0});
-	t1.SetFillColor(Palette::BLUE);
+	easy::graphics::shapes::Triangle t1({ -0.5, -0.5, 0}, { 0.5, -0.5, 0}, { 0, 0.5, 0});
+	t1.SetFillColor(Palette::RED);
 
 	int w, h;
 	glfwGetWindowSize(window, &w, &h);
 
 	std::cout << sizeof(easy::graphics::core::Vertex) << std::endl;
 
+		float degrees = 0;
 	// Main while loop
 	while (!glfwWindowShouldClose(window))
 	{
+		//double xpos, ypos;
+		//glfwGetCursorPos(window, &xpos, &ypos);
+		//
+		//xpos = Normalice(w, xpos);
+		//ypos = -1 *  Normalice(h, ypos);
+		//
 
-		double xpos, ypos;
-		glfwGetCursorPos(window, &xpos, &ypos);
+		float cosr = cos(Degrees2Rads(degrees));
+		float sinr = sin(Degrees2Rads(degrees));
 
-		xpos = Normalice(w, xpos);
-		ypos = -1 *  Normalice(h, ypos);
 
-		t1.P3().X() = xpos;
-		t1.P3().Y() = ypos;
+		float x1 = t1.P1().X() * cosr - t1.P1().Y() * sinr;
+		float y1 = t1.P1().X() * sinr + t1.P1().Y() * cosr;
+
+		float x2 = t1.P2().X() * cosr - t1.P2().Y() * sinr;
+		float y2 = t1.P2().X() * sinr + t1.P2().Y() * cosr;
+
+		float x3 = t1.P3().X() * cosr - t1.P3().Y() * sinr;
+		float y3 = t1.P3().X() * sinr + t1.P3().Y() * cosr;
+
+		t1.P1().X() = x1;
+		t1.P1().Y() = y1;
+
+		t1.P2().X() = x2;
+		t1.P2().Y() = y2;
+
+		t1.P3().X() = x3;
+		t1.P3().Y() = y3;
 
 		// Specify the color of the background
 		glClearColor(0.07f, 0.13f, 0.17f, 1.0f);
@@ -86,6 +112,10 @@ int main()
 		glfwSwapBuffers(window);
 		// Take care of all GLFW events
 		glfwPollEvents();
+
+		//std::this_thread::sleep_for(std::chrono::milliseconds(50));
+
+		degrees += 0.01;
 	}
 
 
